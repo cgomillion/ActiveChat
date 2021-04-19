c272d6d4b96396f78ff9c7d32e8034d3e696a0dd
Gioele Rossi <gioelerossi@Gioeles-Mac-mini.local>
2021-04-19 10:36:45 +0100

    started testing

--- active-chat/src/App.js
+++ active-chat/src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+const SERVER = "http://127.0.0.1:8080";
+const cors = require('cors');
+app.use(cors())
+
+
+console.log(process.env.NODE_ENV)
+let baseUrl = ''
+
+// more on React environment variables
+// https://create-react-app.dev/docs/adding-custom-environment-variables/
+if (process.env.NODE_ENV === 'development') {
+  baseUrl = 'http://localhost:3003'
+} else {
+  baseUrl = 'heroku url here'
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    var socket = socketClient (SERVER);
+
+class App extends Component {
+      constructor(props) {
+        super(props)
+        this.state = {
+        chats: [],
+
+        }
+
+      }
+
+      getChats = () => {
+   // fetch to the backend
+   fetch(baseUrl + "/chats")
+   .then(res => { return res.json()
+   }).then(data => {
+     this.setState({
+       chats: data,
+     })
+    })
+
+     render() {
+        return (
+        <div classname="App"></div>
+            <header></header>
+
+
+                <p>test</p>
+
+
+    );
+‍ }
 }
+
 
 export default App;
