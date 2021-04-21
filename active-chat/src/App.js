288e59df4362cbe1447ae3119741dd882ba64505
cgomillion <cgomillion2@gmail.com>
2021-04-20 23:13:32 -0400

    Got the front end to show something

--- active-chat/src/App.js
+++ active-chat/src/App.js
@@ -1,25 +1,233 @@
-import logo from './logo.svg';
+import socketClient  from "socket.io-client";
+import React, { Component } from 'react'
 import './App.css';
+import Nav from './components/Nav';
+import TopicsTable from './components/TopicsTable';
+import NewForm from './components/NewTopicForm';
+const SERVER = "http://127.0.0.1:8080";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+console.log(process.env.NODE_ENV)
+let baseUrl = ''
+
+// more on React environment variables
+// https://create-react-app.dev/docs/adding-custom-environment-variables/
+if (process.env.NODE_ENV === 'development') {
+  baseUrl = 'http://localhost:3003'
+} else {
+  baseUrl = 'heroku url here'
 }
+
+
+;
+    class App extends Component {
+      
+     
+
+      constructor(props) {
+        super(props)
+        this.state = {
+          topics:[],
+          modalOpen: false,
+          userLogedIn: false
+        }
+
+      }
+
+      getTopics = () => {
+        // fetch to the backend
+        fetch(baseUrl + "/topics",{
+          credentials: "include"
+        })
+        .then(res => {
+          if (res.status===200){
+            return res.json()
+          }
+          else {
+            return []
+          }
+        }).then(data => {
+          this.setState({
+            topics: data,
+          })
+         })
+      }
+
+      addTopic = (newTopic) => {
+        const copyTopic = [...this.state.topics]
+        copyTopic.push(newTopic)
+        this.setState({
+          topics: copyTopic,
+        })
+      }
+
+      loggingUser = async (e) => {
+        console.log('logingUser')
+        e.preventDefault()
+        const url = baseUrl + '/users/login'
+        const logindBody = {
+          username: e.target.username.value,
+          password: e.target.password.value
+        }
+        try {
+
+          const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(logindBody),
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            credentials: "include"
+          })
+
+          if (response.status === 200) {
+            this.getTopics()
+          }
+        }
+        catch (err) {
+          console.log('Error => ', err);
+        }
+      }
+
+      register = async (e) => {
+        e.preventDefault()
+        const url = baseUrl + '/users/signup'
+        try {
+          const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+              username: e.target.username.value,
+              password: e.target.password.value
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          if (response.status === 200) {
+            this.getTopics()
+          }
+        }
+        catch (err) {
+          console.log('Error => ', err);
+        }
+      }
+
+      deleteTopic = async (id) => {
+        const url = baseUrl + '/topics/' + id
+
+        try{
+          const response = await fetch( url, {
+            method: 'DELETE',
+            credentials: "include"
+          })
+
+          if (response.status===200){
+
+            const findIndex = this.state.topics.findIndex(topic => topic._id === id)
+            const copyTopics = [...this.state.topics]
+            copyTopics.splice(findIndex, 1)
+
+            this.setState({
+              topics: copyTopics
+            })
+          }
+
+        }
+        catch(err){
+          console.log('Error => ', err);
+        }
+      };
+
+
+    handleSubmit = async (e) => {
+      e.preventDefault()
+        const url = baseUrl + '/topics/' + this.state.topicToBeEdit._id
+        try{
+          const response = await fetch( url , {
+            method: 'PUT',
+            body: JSON.stringify({
+              name: e.target.name.value,
+              description: e.target.description.values
+            }),
+            headers: {
+              'Content-Type' : 'application/json'
+            },
+            credentials: "include"
+          })
+
+          if (response.status===200){
+            const updatedTopics = await response.json()
+            const findIndex = this.state.topics.findIndex(topics => topics._id === updatedTopics.data._id)
+            const copyTopics = [...this.state.topics]
+            copyTopics[findIndex] = updatedTopics.data
+            this.setState({
+              topics: copyTopics,
+              modalOpen:false
+            })
+          }
+        }
+        catch(err){
+          console.log('Error => ', err);
+        }
+      }
+      componentDidMount() {
+    this.getTopics()
+    var socket = socketClient (SERVER);
+    socket.on('connection', () => {
+        console.log(`I'm connected with the back-end`);
+});
+  }
+
+  handleChange = (e)=>{
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  showEditForm = (topics)=>{
+    this.setState({
+      modalOpen:true,
+      name: topics.name,
+      topicToBeEdit:topics,
+    })
+  }
+
+
+      render () {
+
+        return (
+          <div className="App">
+            <Nav loggingUser={this.loggingUser} register={this.register}/>
+
+              <h1>Test </h1>
+              <NewForm baseUrl={ baseUrl } addTopic={ this.addTopic } />
+
+              <TopicsTable
+                topics={this.state.topics}
+
+                deleteTopic={this.deleteTopic}
+                showEditForm={this.showEditForm}
+                />
+              <br/>
+              <br/>
+
+              {this.state.modalOpen &&
+
+                <form onSubmit={this.handleSubmit}>
+                  <label>Name: </label>
+                  <input name="name" value={this.state.name} onChange={this.handleChange}/> <br/>
+
+                  <label>Description: </label>
+                  <input name="description" value={this.state.description} onChange={this.handleChange}/>
+
+                  <button>submit</button>
+
+                </form>
+              }
+          </div>
+        );
+      }
+    }
 
 export default App;
---
+++ active-chat/src/components/NewTopicForm.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+export default class NewForm extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      name: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (event) {
+    // console.log(event.target.value)
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+    // fetch
+    fetch(this.props.baseUrl + '/topics', {
+      method: 'POST',
+      body: JSON.stringify({name: this.state.name}),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: "include"
+    }).then( res => {
+      return res.json()
+    }).then( data => {
+      this.props.addTopic(data)
+      this.setState({
+        name: ''
+      })
+    }).catch (error => console.error({'Error': error}))
+  }
+
+
+  
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="name">Name: </label>
+        <input type="text" id="name" name="name" onChange={ (evt) => this.handleChange(evt) } value={ this.state.name } />
+        <input type="submit" value="Add a Reason to Celebrate" />
+      </form>
+    )
+  }
+}
